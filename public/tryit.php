1ffc178ad9348bf8d83c62fda9cf7644acf08d48
DDEV User <nobody@example.com>
2025-12-28 15:45:20 +0000

    use the right jpg

--- public/tryit.php
+++ public/tryit.php
@@ -3,6 +3,6 @@
 require_once '../vendor/autoload.php';
 use Jcupitt\Vips\Image;
 
-$image = Image::newFromFile(__DIR__ . '/../../test.jpg');
+$image = Image::newFromFile(__DIR__ . '/images.jpg');
 dump($image);
 die();
