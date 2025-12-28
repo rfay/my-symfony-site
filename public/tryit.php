54e6a63b2235f55de2596d62aa78b89d50a94e38
Randy Fay <randy@randyfay.com>
2025-12-28 08:43:51 -0700

    update tryit

--- public/tryit.php
+++ public/tryit.php
@@ -1,11 +1,8 @@
 <?php
+
+require_once '../vendor/autoload.php';
 use Jcupitt\Vips\Image;
-final class HomeController extends AbstractController {
-    #[Route('/test')]
-    public function test(): Response
-    {
-        $image = Image::newFromFile(__DIR__ . '/images.jpg'); // Unable to open library 'libvips.so.42'. Make sure that you've installed libvips and that 'libvips.so.42' is on your system's library search path.
-        dump($image);
-        die();
-    }
-}
+
+$image = Image::newFromFile(__DIR__ . '/../../test.jpg');
+dump($image);
+die();
